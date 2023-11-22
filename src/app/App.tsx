d220743b6e121182dc9e118bba329f337db772cb
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-11-22 21:32:39 +0200

    restructure files

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+export const App = (): JSX.Element =>  {
+
+  return (
+    <div>App component</div>
+  )
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from 'src/app/App'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const container = document.getElementById('root') as HTMLElement;
+
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
