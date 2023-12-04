28c2b78ebb9d1fc09a90bd760ea155fb369f28d5
Albert <pivnenko.albert@gmail.com>
2023-12-04 22:21:19 +0200

    added some changes ...

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,24 @@
 import { observer } from 'mobx-react-lite';
-import { CoinCard } from 'src/entities/ui/CoinCard/CoinCard';
 import { store } from 'src/shared/store';
+
+import cn from 'classnames';
+import styles from './Dashboard.module.css';
 
 export const Dashboard = observer((): JSX.Element => {
 	return (
-		<div>
+		<div className={cn(styles.container)}>
 			{store.topCoinCollection.map((coin) => (
-				<CoinCard key={coin.CoinInfo.Id} coin={coin} />
+				<div key={coin.CoinInfo.Id}>
+					<div>
+						<span>{coin.DISPLAY.USD.FROMSYMBOL || coin.CoinInfo.Name}</span>
+						<span className={cn(styles.status, {
+							[styles.negative]: coin.DISPLAY.USD.CHANGE24HOUR < 0
+						})}>{coin.DISPLAY.USD.CHANGE24HOUR}</span>
+					</div>
+					<div>
+						{coin.DISPLAY.USD.PRICE}
+					</div>
+				</div>
 			))}
 		</div>
 	);
